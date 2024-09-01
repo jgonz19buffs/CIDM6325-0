d8902aed1f7b42d11918b9d80699693c1524a15b
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-09-01 17:06:29 -0500

    email settings

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from . import key
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL =config('DEFAULT_FROM_EMAIL')
+# The below will write emails to console if you can't use an SMTP server
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
