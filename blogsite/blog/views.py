c1fd8a40f3e7278798f69212c906f677a8636a19
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-09-02 19:07:23 -0500

    stemming and ranking results

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,4 +1,8 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
 from django.db.models import Count
@@ -164,10 +168,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body'),
-                ).filter(search=query)
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                )
+                .filter(search=search_query)
+                .order_by('-rank')
             )
     
     return render(
