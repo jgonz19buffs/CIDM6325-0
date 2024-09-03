de70b7a72e9b03ef7fa4b408bc958a492075aaa7
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-09-03 08:25:40 -0500

    add tags to sitemap with help of ChatGPT, task.md

--- blogsite/blog/sitemaps.py
+++ blogsite/blog/sitemaps.py
@@ -1,4 +1,6 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from taggit.models import Tag
 from .models import Post
 
 class PostSitemap(Sitemap):
@@ -10,3 +12,13 @@ class PostSitemap(Sitemap):
     
     def lastmod(self,obj):
         return obj.updated
+    
+class TagSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.5
+
+    def items(self):
+        return Tag.objects.all()
+    
+    def location(self, item):
+        return reverse('blog:post_list_by_tag', args=[item.slug])
